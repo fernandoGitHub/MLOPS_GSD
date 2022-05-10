3a3302b7a2b33efb8fce57c8419fe540da0b2402
fernandoGitHub <fernando.funtowicz@gmail.com>
2022-05-10 16:13:12 +0300

    Update MLOP_setup.py
    Created beautiful commands

--- MLOP_setup.py
+++ MLOP_setup.py
@@ -1,73 +1,61 @@
-def initialize():
-  import sys
-  import subprocess
-  import pkg_resources
-  
-  
+import sys
+import subprocess as sp
+import pkg_resources
+
+PACKAGE_DICT = {'TENSORFLOW':'tensorflow', 
+                'TF_DATA_VALIDATION':'tensorflow-data-validation',
+                'TF_TRANSFORM':'tensorflow-transform',
+                'TFX':'tfx',
+                'TF_MODEL_ANALYSIS':'tensorflow-model-analysis',
+                'TF_FAIRNESS_INDICATORS':'fairness-indicators',
+                'PIP':'pip'}
+
+
+def display_packages():
+  print (PACKAGE_DICT.keys())
+
+
+def display_installed_packages():
+  print (get_installed_packages())
+
+
+def get_installed_packages():
+  return sorted({pkg.key for pkg in pkg_resources.working_set})
+
+
+def get_package (package_key):
+  return PACKAGE_DICT.get (package_key, 'Not Found')
+
 
 def is_installed (package):
   # Getting all the list of packages
-  installed_packages = {pkg.key for pkg in pkg_resources.working_set}
-  return (package in installed_packages)
+  return (package in get_installed_packages())
 
-def install_package (package):
-  initialize()
-  # implement pip as a subprocess:
-  subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
- 
-def upgrade_package (package):
-  initialize()
-  # implement pip as a subprocess:
-  subprocess.check_call([sys.executable, '-m', 'pip', 'upgrade', package])
-  
-  
-def update_PIP():
-  try:
-    import colab
-    print ('Upgrading PIP to latest version')
-    #!pip install --upgrade pip
-  except:
-    pass
-  
-def install_TensorFlow_Data_Validation (reload = True):
-  print ('Installing TensorFlow Data Validation - Latest')
-  #!pip install tensorflow_data_validation
-  if reload:
-    reload_packages()
-  
-def install_TensorFlow_Data_ValidationLessThan2 (reload = True):
-  print ('Installing TensorFlow Data Validation')
-  #!pip install --upgrade 'tensorflow_data_validation[visualization]<2'
-  if reload:
-    reload_packages()
-  
-def install_TensorFlow_Transform (reload = True):
-  print ('Installing TensorFlow Transform')
-  #!pip install -q -U tensorflow_transform
+
+def install_package(package_key, reload = True):
+
+  package = get_package(package_key)
+  if (package == 'Not Found'):
+    print ('Wrong Key! Install will be skipped')
+    return
+
+  # Veryfing existing install prior to install
+  if is_installed(package):
+    print (f'Package: {package} is already installed. Skipping installation')
+  else:
+    print(f'Installing {package}')
+    sp.check_call([sys.executable, '-m', 'pip', 'install', package])
+    print(f'Package {package} has been successfully installed')
+
   if reload:
     reload_packages()
 
-def install_TensorFlow_Model_Analysis (reload = True):
-  print ('Installing TensorFlow Model Analysis')
-  #!pip install tensorflow-model-analysis
-  if reload:
-    reload_packages()
-  
-def install_TFX (reload = True):
-  print ('Installing TFX Pipelines')
-  #!pip install -U tfx
-  if reload:
-    reload_packages()
-  
-def install_Fairness_Indicators (reload == true):
-  print ('Installing several libraries including TFDV and TFMA')
-  #!pip install fairness-indicators
-    if reload:
-    reload_packages()
-  
+
 def reload_packages():
   print ('Reloading Packages')
   import pkg_resources
   import importlib
   importlib.reload(pkg_resources)
+  
+
   
