208fa5c70172e48ac6910965aacba31a55e9729c
fernandoGitHub <fernando.funtowicz@gmail.com>
2022-05-11 10:28:19 +0300

    Update TF_stat.py

--- TF_stat.py
+++ TF_stat.py
@@ -1,6 +1,7 @@
 import tensorflow_data_validation as tfdv
 import os
 from tensorflow_data_validation.utils import slicing_util
+from types import NoneType
 
 
 def set_slicing_feature(feature_name):
